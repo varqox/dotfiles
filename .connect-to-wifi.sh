b89c9e73e00f6715333ae525122171a48915a013
Krzysztof Małysa <varqox@gmail.com>
2023-01-31 20:28:09 +0100

    connecting to wifi: fixed display order to be more useful

--- .connect-to-wifi.sh
+++ .connect-to-wifi.sh
@@ -3,8 +3,7 @@ wifi_name=$(iwctl station wlan0 scan > /dev/null; iwctl station wlan0 get-networ
 	tail -n +5 | # remove table headers
 	sed "s/\x1b\[\(0m\|1;90m\**\)//g" | # remove colors
 	sed "s/^......//" | grep -Pv "^$" | # remove padding and prompt
-	sort --general-numeric-sort --ignore-case |
-	fzf --reverse --bind 'load:reload:iwctl station wlan0 scan > /dev/null; iwctl station wlan0 get-networks | tail -n +5 | sed "s/\x1b\[\(0m\|1;90m\**\)//g" | sed "s/^......//" | grep -Pv "^$" | sort --general-numeric-sort --ignore-case; sleep 0.3' |
+	fzf --reverse --no-sort --bind 'load:reload-sync:sleep 0.3; iwctl station wlan0 scan > /dev/null; iwctl station wlan0 get-networks | tail -n +5 | sed "s/\x1b\[\(0m\|1;90m\**\)//g" | sed "s/^......//" | grep -Pv "^$"' |
 	sed 's/  *\([[:graph:]]*\)  *\(\*\**\) *$//g') # extract network name by removing everything after the network name (space-proof method)
 if [ ! -z "${wifi_name}" ]; then
 	echo "Connecting to: ${wifi_name}..."
