ce37cae2167550e473cd893444e39df70befa995
Krzysztof Małysa <varqox@gmail.com>
2023-08-13 21:51:18 +0200

    zsh: switched from oh-my-zsh to zsh4humans

--- zsh/configure.sh
+++ zsh/configure.sh
@@ -2,17 +2,14 @@
 set -e
 source "$(dirname -- "$0")/../common.sh"
 
-print_step "zsh: install oh-my-zsh-git"
-paruS oh-my-zsh-git
-
 print_step "zsh: install used utilities"
 paruS neovim ccache clang gcc exo procps-ng gdb git meson ffmpeg speedtest-cli fzf bat
 
 print_step "zsh: copy configs"
 safe_copy --link zshrc "$HOME/.zshrc"
-
-print_step "zsh: create ~/.cache directory for oh-my-zsh"
-mkdir -p "$HOME/.cache"
+safe_copy --link zshenv "$HOME/.zshenv"
+safe_copy --link p10k.zsh "$HOME/.p10k.zsh"
+safe_copy --link zshrc.local "$HOME/.zshrc.local"
 
 print_step "zsh: change shell to zsh"
 [ "$SHELL" = "/usr/bin/zsh" ] || sudo chsh -s /usr/bin/zsh "${USER}"
