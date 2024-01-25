ae6dc8e90fc3732032b9aabbc413e4cbb0494c86
Krzysztof Małysa <varqox@gmail.com>
2024-01-26 00:24:12 +0100

    mpv: fix autosub + remove broken opensubtitles backend

--- mpv/install-plugin-autosub.sh
+++ mpv/install-plugin-autosub.sh
@@ -5,25 +5,12 @@ source "$(dirname -- "$0")/../common.sh"
 print_step "mpv: autosub plugin: install subliminal"
 paruS subliminal
 
-print_step "mpv autosub plugin: decrypt opensubtitles_credentials.age"
-tmp_paruS age age-plugin-yubikey pcsclite
-sudo systemctl start pcscd.socket
-
-opensubtitles_credentials="--          { '--opensubtitles', 'USERNAME', 'PASSWORD' },"
-if [ ! -z "$(age-plugin-yubikey --list)" ] && [ -f "${XDG_CONFIG_HOME:-$HOME/.config}/age-yubikey-identity.txt" ]; then
-	opensubtitles_credentials=$(age --decrypt --identity "${XDG_CONFIG_HOME:-$HOME/.config}/age-yubikey-identity.txt" opensubtitles_credentials.age)
-	# To encrypt use:
-	# age --encrypt --identity ~/.config/age-yubikey-identity.txt -o opensubtitles_credentials.age opensubtitles_credentials
-else
-	warn "Decrypting of opensubtitles_credentials.age skipped due to a missing YubiKey and age setup"
-fi
-
 function change_subliminal_path() {
 	sed "s@^\(local subliminal = \).*@\1'/usr/bin/subliminal'@"
 }
 
-function enable_searching_opensubtitles() {
-	sed "s@^--          { '--opensubtitles', 'USERNAME', 'PASSWORD' },@  ${opensubtitles_credentials}@"
+function disable_automatic_subtitle_download() {
+	sed "s@^    auto = [^,]*,@    auto = false,@"
 }
 
 function disable_dutch_language() {
@@ -42,9 +29,9 @@ print_step "mpv: install plugin autosub"
 safe_copy <(
 		cat scripts/autosub/autosub.lua |
 		change_subliminal_path |
+		disable_automatic_subtitle_download |
 		disable_dutch_language |
 		enable_polish_language |
-		enable_searching_opensubtitles |
 		change_shortcut_for_download_subs2
 	) "${XDG_CONFIG_HOME:-$HOME/.config}/mpv/scripts/autosub/autosub.lua"
 safe_copy <(echo "require('autosub')") "${XDG_CONFIG_HOME:-$HOME/.config}/mpv/scripts/autosub/main.lua"
