0e6ae82a518600a5704cf0fc96833c2e775b87e3
Krzysztof Małysa <varqox@gmail.com>
2024-06-19 10:45:05 +0200

    pipewire: run wireplumber after installation

--- pipewire/install.sh
+++ pipewire/install.sh
@@ -4,3 +4,6 @@ source "$(dirname -- "$0")/../common.sh"
 
 print_step "pipewire: install PipeWire and WirePlumber"
 paruS pipewire-pulse pipewire-alsa wireplumber
+
+print_step "pipewire: start WirePlumber"
+systemctl --user start wireplumber
