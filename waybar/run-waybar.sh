db71fccbc8addfc5348e980d02e31116c927fb35
Krzysztof Małysa <varqox@gmail.com>
2024-06-19 10:46:07 +0200

    waybar: fix detecting AC at startup on Framework 16

--- waybar/run-waybar.sh
+++ waybar/run-waybar.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 external_display_exists=$([ "$(swaymsg -t get_outputs --pretty --raw | grep '^    "subpixel_hinting":' -c)" = "1" ] || echo yes)
-is_ac_connected=$([ "$(cat /sys/class/power_supply/AC/online)" = "1" ] && echo yes)
+# On Lenovo Thinkpad E480 the directory is AC/, on Framework 16 it is ACDC
+is_ac_connected=$([ "$(cat /sys/class/power_supply/AC*/online)" = "1" ] && echo yes)
 
 if [ "${external_display_exists}" = yes ] || [ "${is_ac_connected}" = yes ]; then
     start_activated="true" # inhibit idle
