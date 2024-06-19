197691a6aeb6480a43ddb293f4b8a5cbbe2a5ca1
Krzysztof Małysa <varqox@gmail.com>
2024-06-19 10:43:33 +0200

    age: fix yubikeys being unrecognized

--- age/install_and_configure.sh
+++ age/install_and_configure.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 source "$(dirname -- "$0")/../common.sh"
 
 print_step "age: install age and age-plugin-yubikey"
-paruS age age-plugin-yubikey pcsclite
+paruS age age-plugin-yubikey pcsclite yubikey-manager
 sudo systemctl enable --now pcscd.socket
