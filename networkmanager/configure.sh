45db9968b98b63118c820990b8b10338517f2846
Krzysztof Małysa <varqox@gmail.com>
2024-06-19 10:44:40 +0200

    networkmanager: fix installation errors by waiting for the internet connection to be restored

--- networkmanager/configure.sh
+++ networkmanager/configure.sh
@@ -7,3 +7,7 @@ paruS iwd
 
 print_step "networkmanager: setup NetworkManager to use IWD"
 sudo_safe_copy wifi_backend.conf '/etc/NetworkManager/conf.d/wifi_backend.conf'
+sudo systemctl restart NetworkManager.service 
+
+print_step "networkmanager: wait for the internet connection to be restored"
+until ping -c 1 archlinux.org; do sleep 0.1; done
--- networkmanager/install.sh
+++ networkmanager/install.sh
@@ -8,3 +8,6 @@ paruS networkmanager
 print_step "networkmanager: enable and restart NetworkManager"
 sudo systemctl enable NetworkManager.service
 sudo systemctl restart NetworkManager.service
+
+print_step "networkmanager: wait for the internet connection to be restored"
+until ping -c 1 archlinux.org; do sleep 0.1; done
