c294b77ab020ca6410932a37fb24db40d5d16e90
Krzysztof Małysa <varqox@gmail.com>
2024-06-22 23:31:04 +0200

    mpv: enable higher configuration everywhere except my i5-8250U machine

--- mpv/configure.sh
+++ mpv/configure.sh
@@ -3,7 +3,15 @@ set -euo pipefail
 source "$(dirname -- "$0")/../common.sh"
 
 print_step "mpv: copy configs"
-safe_copy --link mpv.conf "${XDG_CONFIG_HOME:-$HOME/.config}/mpv/mpv.conf"
+if cat /proc/cpuinfo | grep '^model name' | grep 'i5-8250U' --quiet; then
+	cp mpv.conf "${tmp_dir}/mpv.conf"
+	sed -i 's/^profile=gpu-hq/#profile=gpu-hq/' "${tmp_dir}/mpv.conf"
+	sed -i 's/^scale=ewa_lanczossharp/#scale=ewa_lanczossharp/' "${tmp_dir}/mpv.conf"
+	sed -i 's/^cscale=ewa_lanczossharp/#cscale=ewa_lanczossharp/' "${tmp_dir}/mpv.conf"
+	safe_copy "${tmp_dir}/mpv.conf" "${XDG_CONFIG_HOME:-$HOME/.config}/mpv/mpv.conf"
+else
+	safe_copy --link mpv.conf "${XDG_CONFIG_HOME:-$HOME/.config}/mpv/mpv.conf"
+fi
 safe_copy --link input.conf "${XDG_CONFIG_HOME:-$HOME/.config}/mpv/input.conf"
 
 print_step "mpv: enable hardware acceleration via VA-API"
