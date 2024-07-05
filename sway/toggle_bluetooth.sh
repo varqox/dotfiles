ac3959ee7a26c2b0dd8bbb3c90594b16ead5f05b
Krzysztof Małysa <varqox@gmail.com>
2024-07-05 13:39:15 +0200

    sway: fix toggling bluetooth

--- sway/toggle_bluetooth.sh
+++ sway/toggle_bluetooth.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-bluetooth | grep "= on" && until bluetoothctl power on; do sleep 0.1; done
+if bluetoothctl show | grep 'Powered: no'; then
+	until bluetoothctl power on; do sleep 0.1; done
+else
+	until bluetoothctl power off; do sleep 0.1; done
+fi
