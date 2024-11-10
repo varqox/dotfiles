b5e649e96f89087f20b93a58892058c13cfb49f6
Krzysztof Małysa <varqox@gmail.com>
2024-11-10 18:52:35 +0100

    scripts/connect-to-wifi.sh: fix message when there are no networks

--- scripts/connect-to-wifi.sh
+++ scripts/connect-to-wifi.sh
@@ -2,8 +2,9 @@
 wifi_name=$(iwctl station wlan0 scan > /dev/null; iwctl station wlan0 get-networks |
 	tail -n +5 | # remove table headers
 	sed "s/\x1b\[\(0m\|1;90m\**\)//g" | # remove colors
-	sed "s/^......//" | grep -Pv "^$" | # remove padding and prompt
-	fzf --reverse --no-sort --bind 'load:reload-sync:sleep 0.3; iwctl station wlan0 scan > /dev/null; iwctl station wlan0 get-networks | tail -n +5 | sed "s/\x1b\[\(0m\|1;90m\**\)//g" | sed "s/^......//" | grep -Pv "^$"' --bind 'down:unbind(load)+down' --bind 'up:unbind(load)+up' |
+	grep -v '^No networks available$' | # remove message when there are no networks
+	sed "s/^......//" | grep -v "^$" | # remove padding and prompt
+	fzf --reverse --no-sort --bind 'load:reload-sync:sleep 0.3; iwctl station wlan0 scan > /dev/null; iwctl station wlan0 get-networks | tail -n +5 | sed "s/\x1b\[\(0m\|1;90m\**\)//g" | grep -v "^No networks available\$" | sed "s/^......//" | grep -v "^$" || true' --bind 'down:unbind(load)+down' --bind 'up:unbind(load)+up' |
 	sed 's/  *\([[:graph:]]*\)  *\(\*\**\) *$//g') # extract network name by removing everything after the network name (space-proof method)
 if [ ! -z "${wifi_name}" ]; then
 	echo "Connecting to: ${wifi_name}..."
