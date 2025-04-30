f273f3689358caec5ced7e70e720e248466cf3b6
Krzysztof Małysa <varqox@gmail.com>
2025-04-30 13:14:18 +0200

    dns: fix clients using /etc/resolv.conf bypassing global settings

--- dns/install_and_configure.sh
+++ dns/install_and_configure.sh
@@ -9,3 +9,6 @@ sudo_safe_copy my.conf "/etc/systemd/resolved.conf.d/my.conf"
 print_step "dns: enable systemd-resolved"
 sudo systemctl enable systemd-resolved.service
 sudo systemctl restart systemd-resolved.service
+
+print_step "dns: migrate clients using /etc/resolv.conf to systemd-resolved"
+sudo ln -sf ../run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
