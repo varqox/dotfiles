be584fd6e2925ebd2ec335b82c73d4fd3adb2828
Krzysztof Małysa <varqox@gmail.com>
2025-12-22 15:13:52 +0100

    sway: make all warnings about dotfiles appear together

--- sway/warn_about_dotfiles_change.sh
+++ sway/warn_about_dotfiles_change.sh
@@ -2,5 +2,8 @@
 set -euo pipefail
 cd "$HOME/dotfiles"
 git update-index --refresh > /dev/null || true
-git diff-index --quiet HEAD -- || swaynag -t warning -m 'dotfiles changed, for backup purposes review, commit and push the changes'
+git diff-index --quiet HEAD -- || {
+	swaynag -t warning -m 'dotfiles changed, for backup purposes review, commit and push the changes'&
+	trap wait EXIT
+}
 [ "$(git rev-parse origin/main)" = "$(git rev-parse HEAD)" ] || swaynag -t warning -m 'changes of dotfiles are not pushed'
