f11737f1e4dc477ee6413b930fd6f33715af16b4
Krzysztof Małysa <varqox@gmail.com>
2026-03-18 12:49:42 +0100

    scripts: add translate-subs

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,3 +9,7 @@ safe_copy --link connect-to-wifi.sh "$HOME/.local/bin/connect-to-wifi.sh"
 print_step "scripts: install connect-bluetooth.sh"
 paruS bluez-utils fzf
 safe_copy --link connect-bluetooth.sh "$HOME/.local/bin/connect-bluetooth.sh"
+
+print_step "scripts: install translate-subs"
+paruS python-selenium
+safe_copy --link translate-subs "$HOME/.local/bin/translate-subs"
